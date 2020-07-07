37a5b7be5f422441aed810cc0d03dd986161d4d5
Kos Terekhin <terekhin.konst@gmail.com>
2020-07-07 16:46:43 +0300

    add book feature

--- client/src/redux/actions/global-variables.js
+++ client/src/redux/actions/global-variables.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://book-crud-practice.herokuapp.com/api';
-// export const BASE_URL = 'http://localhost:5000/api';
+// export const BASE_URL = 'https://book-crud-practice.herokuapp.com/api';
+export const BASE_URL = 'http://localhost:5000/api';
 export const CLIENT = '@@CLIENT';
 export const SINGLE_BOOK_TEXT_TOUPDATE = [ '_id', 'title', 'subtitle', 'isbn13', 'price' ];
