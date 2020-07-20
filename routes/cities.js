e3cd9ef7d8e16f35decd04f2c1c8fa89c4e5b40f
Kos Terekhin <terekhin.konst@gmail.com>
2020-07-20 11:26:24 +0300

    updated cities data set

--- routes/cities.js
+++ routes/cities.js
@@ -3,28 +3,13 @@ const router = express.Router();
 const cities = require('cities.json');
 
 router.get('/', async (req, res) => {
-	// const res = cities.filter((city) => )
+	const filterList = [ 'UA' ];
 
-	const regex = new RegExp('pari', 'gi');
+	const regex = new RegExp(`^${req.body}`, 'gi');
 
-	const data = cities.filter(({ name }) => name.match(regex));
+	const data = cities.filter(({ country }) => filterList.includes(country)).filter(({ name }) => name.match(regex));
 
 	return res.json(data);
-
-	// try {
-	// 	let data = await Books.find({});
-	// 	let partialData = data.map((item) => {
-	// 		return {
-	// 			_id: item._id,
-	// 			title: item.title,
-	// 			images: [ item.images[0] ]
-	// 		};
-	// 	});
-
-	// 	return res.json(partialData);
-	// } catch (error) {
-	// 	return res.status(500).send('Internal Server Error');
-	// }
 });
 
 module.exports = router;
